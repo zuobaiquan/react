d75d6a30f5f8c049ae2a54127d1a05f179d0f2f6
左佰全 <zuobaiquan@ipzoe.com>
2017-08-01 22:41:26 +0800

    update

--- react基本环境安装/hello-react/src/CommentInput.js
+++ react基本环境安装/hello-react/src/CommentInput.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react'
 
 class CommentInput extends Component {
+  constructor () {
+    super();
+    this.state = {
+      username: '',
+      content: ''
+    }
+  }
+  handleUsernameChange (event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+  handleContentChange (event) {
+    this.setState({
+      content: event.target.value
+    })
+  }
+  handleSubmit () {
+    if (this.props.onSubmit) {
+      const { username, content } = this.state
+      this.props.onSubmit({username, content})
+    }
+    //为了用户体验，保留输入的用户名，清空评论内容
+    this.setState({ content: '' })
+  }
   render () {
     return (
       <div className='comment-input'>
         <div className='comment-field'>
           <span className='comment-field-name'>用户名：</span>
           <div className='comment-field-input'>
-            <input />
+            <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)}  />
           </div>
         </div>
         <div className='comment-field'>
           <span className='comment-field-name'>评论内容：</span>
           <div className='comment-field-input'>
-            <textarea />
+            <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)} />
           </div>
         </div>
         <div className='comment-field-button'>
-          <button>发布</button>
+          <button onClick={this.handleSubmit.bind(this)}>发布</button>
         </div>
       </div>
     )
