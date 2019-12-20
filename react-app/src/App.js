2656958c09011651a4cb0480926aa28fc56ea483
zuobaiquan <zuobaiquan01@gmail.com>
2019-12-20 16:32:35 +0800

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      111
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    a: 1
+  };
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <p>{this.state.a}</p>
+        <button
+          onClick={() => {
+            this.setState({ a: 1 }); // 这里并没有改变 a 的值
+          }}
+        >Click me</button>
+        <button onClick={() => this.setState(null)}>setState null</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- reatc相关文档/性能！！让你的 React 组件跑得再快一点.md
+++ reatc相关文档/性能！！让你的 React 组件跑得再快一点.md
@@ -25,18 +25,11 @@ class App extends React.Component {
   render() {
     console.log("render");
     return (
-      <React.Fragement>
+      <div>
         <p>{this.state.a}</p>
-        <button
-          onClick={() => {
-            this.setState({ a: 1 }); // 这里并没有改变 a 的值
-          }}
-        >
-          Click me
-        </button>
+        <button onClick={() => { this.setState({ a: 1 }); // 这里并没有改变 a 的值  }}>Click me</button>
         <button onClick={() => this.setState(null)}>setState null</button>
-        <Child />
-      </React.Fragement>
+      </div>
     );
   }
 }
