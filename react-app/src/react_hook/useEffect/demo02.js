ca8367bc8444d3594e9543132a688acaf43c92ce
zuobaiquan <zuobaiquan01@gmail.com>
2019-12-30 18:15:48 +0800

    update

--- react-app/src/react_hook/useEffect/demo02.js
+++ react-app/src/react_hook/useEffect/demo02.js
@@ -26,10 +26,12 @@ function App () {
             window.addEventListener('resize',onResize,false);
         }
     },[]);
+    // ******  useEffect 精髓   ******
     //上面useEffect函数的空数组的参数，其作用是用于比对。决定该 useEffect 是否执行
     // 如果第二个参数不写，则每次都会执行这个 useEffect ，如果为空数组，则只执行一次
-    // 如果数组中写了数据，则比对每一个数据，只有数组中的每一项都不变的情况下，才会再次执行；
+    // 如果数组中写了数据，则比对每一个数据，只有数组中的每一项都不变的情况下，才不会再次执行；
     // 如下面，变化size 不会触发下面useEffect的函数执行
+    // 第二个参数 可选数组，只有数组中的每一项都不变的情况下，才不会再次执行
     useEffect(()=>{
         console.log(count);
     },[count])
@@ -43,5 +45,5 @@ export default App;
 
 //总结：为了实现初始化时和数据更新时，title发生变化，同样的交互代码要在两个生命周期中执行两次，类似的 再加上 监听函数，需要在卸载生命周期中 去掉卸载函数;
 // 如果使用使用 effect hooks
-
-
+//
+// userEffect函数是在 render之后调用的 ，其功能相当于 componentDidMount/和componentDidUpdate，并且该函数有callback函数，其功能是清除上一次副作用 遗留下来的状态 相当于componentWillUnmount
