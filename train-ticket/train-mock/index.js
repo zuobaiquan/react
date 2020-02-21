fdeb6858e3f684abc1d8d19a980538a184f392bf
zuobaiquan <zuobaiquan01@gmail.com>
2020-02-21 17:29:46 +0800

    111

---
+++ train-ticket/train-mock/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+    response.status(200);
+    response.send('hello express');
+    response.end();
+});
+
+app.get('/rest', (request, response) => {
+    response.json({
+        result: 1,
+        msg: 'hello express',
+    });
+    response.end();
+});
+app.listen(5000);
