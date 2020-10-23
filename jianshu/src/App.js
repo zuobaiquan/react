d9e79b780dfc6686b5a34dc927f483b804c30963
zuobaiquan <zuobaiquan@qq.com>
2020-10-23 22:09:00 +0800

    update

---
+++ jianshu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Globalstyle } from './style'
+import Header from './common/header'
+function App() {
+  return (
+    <>
+      <Header />
+      <Globalstyle />
+    </>
+  );
+}
+
+export default App;
