55a5f300eca189cedadc4abeee685aac244dfcce
zuobaiquan <zuobaiquan@qq.com>
2020-10-25 21:27:08 +0800

    update

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Globalstyle } from './style'
+import { IconFontstyle } from './statics/iconfont/iconfont'
 import Header from './common/header'
 function App() {
   return (
     <>
       <Header />
       <Globalstyle />
+      <IconFontstyle />
     </>
   );
 }
--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
+import {
+  HeaderWraper,
+  Logo,
+  Nav,
+  NavItem,
+  SearchWrapper,
+  SearchInput,
+  WriteBook
+} from './style'
 class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <HeaderWraper>
+        <Logo />
+        <Nav>
+          <NavItem className='active'>首页</NavItem>
+          <NavItem>下载App</NavItem>
+          <SearchWrapper>
+            <SearchInput></SearchInput>
+            <span className="iconfont">&#xe64d;</span>
+          </SearchWrapper>
+        </Nav>
+        <WriteBook><span className="iconfont">&#xe604;</span>写文章</WriteBook>
+      </HeaderWraper>
     )
   }
 }
