f640f56cb9fe0c61ea096718e49fcba39a121441
zuobaiquan <zuobaiquan@qq.com>
2020-12-27 20:51:45 +0800

    update

--- reactui-typescript/src/App.tsx
+++ reactui-typescript/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './styles/index.scss';
 import Hello from './Hook/Hello'
-import Button, { ButtonSize, ButtonType } from './component/button'
+import Button from './component/button'
 
 function App() {
   return (
     <div className="App">
       <Hello message='测试message'><span>111</span></Hello>
-      <Button btnType={ButtonType.Default} size={ButtonSize.Large}>测试</Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>测试</Button>
-      <Button btnType={ButtonType.Link} target="_blank" href="www.baidu.com">百度</Button>
+      <Button btnType='default' size="lg">测试</Button>
+      <Button btnType="danger" size="sm">测试</Button>
+      <Button btnType="link" target="_blank" href="http://www.baidu.com">百度</Button>
     </div>
   );
 }
--- reactui-typescript/src/component/button/index.tsx
+++ reactui-typescript/src/component/button/index.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from 'react'
 import classNames from 'classnames'
 
-export enum ButtonSize {
-  Large = 'lg',
-  Small = 'sm'
-}
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+// export enum ButtonSize {
+//   Large = 'lg',
+//   Small = 'sm'
+// }
 
-export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link'
-}
+// export enum ButtonType {
+//   Primary = 'primary',
+//   Default = 'default',
+//   Danger = 'danger',
+//   Link = 'link'
+// }
 
 interface BaseButtonProps {
   className?: string;
@@ -31,9 +33,9 @@ const Button: FC<ButtonProps> = (props) => {
   const classes = classNames('btn', {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    'disabled': (btnType === ButtonType.Link) && disabled
+    'disabled': (btnType === 'link') && disabled
   })
-  if (btnType === ButtonType.Link && href) {
+  if (btnType === 'link' && href) {
     return (
       <a className={classes} href={href} {...restProps}>
         {children}
@@ -54,6 +56,6 @@ const Button: FC<ButtonProps> = (props) => {
 }
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default
+  btnType: "default"
 }
 export default Button
